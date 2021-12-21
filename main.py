da87d70e79982a64cb96afd33239eef9fd479387
MaxxAferist <maxim.lev.8@mail.ru>
2021-12-21 21:31:52 +0300

    Класс для карт v.0_1

--- main.py
+++ main.py
@@ -1,22 +1,69 @@
 import pygame
 import sys
+import os
+from ctypes import *
+
+
+def load_image(name):
+    filename = os.path.join('data', name)
+    if not os.path.isfile(filename):
+        print(f'Image is not found: {filename}')
+        sys.exit()
+    image = pygame.image.load(filename)
+    return image
 
 
 def termit():
     pygame.quit()
     sys.exit()
 
+
+class Card(pygame.sprite.Sprite):
+    def __init__(self, value, suit, pos):
+        super().__init__(all_sprites, card_sprites)
+        self.value = value
+        self.suit = suit
+        self.image = load_image(f'cards//{self.value}_{self.suit}.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.motion_go = False
+
+    def update(self, *args):
+        if args:
+            event = args[0]
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and \
+                    self.rect.collidepoint(event.pos):
+                self.motion_go = True
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                self.motion_go = False
+            if event.type == pygame.MOUSEMOTION and self.motion_go and \
+                    self.rect.collidepoint(event.pos):
+                self.rect.x += event.rel[0]
+                self.rect.y += event.rel[1]
+
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Poker')
     size = 0, 0
+    width, height = windll.user32.GetSystemMetrics(0), windll.user32.GetSystemMetrics(1)
     screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
     running = True
+    FPS = 60
     clock = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
+    card_sprites = pygame.sprite.Group()
 
     while running:
+        event = None
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 termit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                Card('2', 'cherv', event.pos)
+            all_sprites.update(event)
         screen.fill(pygame.Color('darkslategray'))
+        all_sprites.draw(screen)
         pygame.display.flip()
+        clock.tick(FPS)
