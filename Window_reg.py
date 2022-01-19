0324fb8157e560fefdb3ea54c62c2c22ed742d73
Artem <mc.Temka2018@mail.ru>
2022-01-19 20:06:09 +0300

    Регистрация(тестовая версия)

---
+++ Window_reg.py
@@ -0,0 +1,50 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Window_reg(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 250)
+        MainWindow.setStyleSheet("")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 480, 50))
+        self.label.setStyleSheet("font: 25 16pt \"Microsoft YaHei Light\";")
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(220, 60, 150, 25))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(220, 100, 150, 25))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(30, 60, 181, 25))
+        self.label_2.setStyleSheet("font: 25 16pt \"Microsoft YaHei Light\";")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(30, 100, 181, 25))
+        self.label_3.setStyleSheet("font: 25 16pt \"Microsoft YaHei Light\";")
+        self.label_3.setObjectName("label_3")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(290, 140, 191, 41))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:22pt;\">Регистрация</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p align=\"right\"><span style=\" font-size:16pt;\">Логин</span></p></body></html>"))
+        self.label_3.setText(_translate("MainWindow", "<html><head/><body><p align=\"right\"><span style=\" font-size:16pt;\">Пароль</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "Зарегистрироваться"))
--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import pygame
+import sys
 from menu import *
+from Window_reg import Window_reg
+from Window_start import Window_start
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow, Window_start):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.start)
+        self.pushButton_2.clicked.connect(self.go_reg)
+
+    def start(self):
+        pygame.init()
+        start_menu = Menu()
+        start_menu.run()
+
+    def go_reg(self):
+        ex1 = Reg_Window()
+        ex1.show()
+
+
+class Reg_Window(QMainWindow, Window_reg):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
-    pygame.init()
-    start_menu = Menu()
-    start_menu.run()
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
