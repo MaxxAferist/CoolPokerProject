6e8bf2d48ea2b1ab13c45a163f3bbcb6a4f4bb98
MaxxAferist <maxim.lev.8@mail.ru>
2022-01-24 20:59:57 +0300

    Все готово v_3.3

--- mini_menu.py
+++ mini_menu.py
@@ -23,11 +23,11 @@ class Mini_menu():
     def add_buttons(self, other):
         left_top = 30 * KOEF
         up_top = 20 * KOEF
-        w_btn = (self.fon.rect.w - left_top * 2) / KOEF
+        w_btn = (self.fon.rect.w - left_top * 2)
         h_btn = w_btn * 0.37
         self.buttons = [Button('Продолжить', (left_top, up_top), (w_btn, h_btn), 43, self.close),
-                        Button('Настройки', (left_top, up_top + h_btn + 20 * KOEF), (w_btn, h_btn), 43, lambda: go_settings(other)),
-                        Button('Выйти', (left_top, up_top + h_btn * 2 + 40 * KOEF), (w_btn, h_btn), 43, lambda: other.go_menu(other.player.money, other.user))]
+                        Button('Настройки', (left_top, up_top * 2 + h_btn), (w_btn, h_btn), 43, lambda: go_settings(other)),
+                        Button('Выйти', (left_top, up_top * 3 + h_btn * 2), (w_btn, h_btn), 43, lambda: other.go_menu(other.player.money, other.user))]
         self.btn_sprites.add(self.buttons)
 
     def close(self):
